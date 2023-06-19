020255ad0efa66d063a00023cdb5fc6f2cfb7037
Yusuf Yildirim <yusufyildirim1025@gmail.com>
2023-06-19 16:07:44 +0300

    -m"created basic kafka config classes."

--- QuoteProducer/src/main/java/edu/marun/quoteproducer/QuoteProducerApplication.java
+++ QuoteProducer/src/main/java/edu/marun/quoteproducer/QuoteProducerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class QuoteProducerApplication {
@@ -15,13 +16,16 @@ public class QuoteProducerApplication {
     }
 
     @Bean
-    CommandLineRunner runner(QuoteRepository quoteRepository) {
+    CommandLineRunner runner(QuoteRepository quoteRepository, KafkaTemplate<String, String> kafkaTemplate) {
         return args -> {
             Quote quote = Quote.builder()
                     .quote("The best way to predict the future is to create it.")
                     .build();
-            Quote saved = quoteRepository.save(quote);
-            System.out.println(quoteRepository.findById(saved.getId()));
+            //Quote saved = quoteRepository.save(quote);
+            //System.out.println(quoteRepository.findById(saved.getId()));
+            kafkaTemplate.send("quotes", "Hello Kafka");
+            Thread.sleep(4000);
+            System.out.println("Message sent");
         };
     }
 
---
+++ QuoteProducer/src/main/java/edu/marun/quoteproducer/config/KafkaTopicConfig.java
@@ -0,0 +1,18 @@
+package edu.marun.quoteproducer.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+    @Bean
+    public NewTopic quotesTopic() {
+        return TopicBuilder
+                .name("quotes")
+                //.partitions(3)
+                //.replicas(1)
+                .build();
+    }
+}
